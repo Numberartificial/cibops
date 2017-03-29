d8606b6d3a60c62b7a552bd1c79f8fbf406afe5d
边城道馆 <248866053@qq.com>
2017-03-29 15:26:50 +0800

    reusable people fetch module demo

--- src/Try/OrdinaryHttp.elm
+++ src/Try/OrdinaryHttp.elm
@@ -1,12 +1,15 @@
 module OrdinaryHttp exposing (..)
 
-import Html exposing (Html, div, button, text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Http
 import Task exposing (Task)
 import Json.Decode as Decode
+import RemoteData exposing (WebData, RemoteData(..))
 
-import Types exposing (People)
+import Styles exposing (..)
+import Types exposing (People, Person)
 import Decoders exposing (decodePeople)
 
 
@@ -14,71 +17,110 @@ import Decoders exposing (decodePeople)
 
 
 type alias Model =
-    String
+    { title : String
+    , people : WebData People
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( "", Cmd.none )
+    ( { title = "http chain"
+      , people = NotAsked
+      }
+    , Cmd.none
+    )
 
 
 
 -- MESSAGES
 
+
 {-| should use this kind of Msg, Task.attempt/perform only accept one
-       parameter to represent a task's result and return a Msg which is dispatch from the result-}
+       parameter to represent a task's result and return a Msg which is dispatch from the result
+-}
 type Msg
     = Fetch
-    | FetchMsg (Result Http.Error String)
-    -- | FetchSuccess String
-    -- | FetchError Http.Error
+    | FetchMsg (Result Http.Error People)
 
 
 
+-- | FetchSuccess String
+-- | FetchError Http.Error
 -- VIEW
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Fetch ] [ text "Fetch" ]
-        , text model
+        [button [ onClick Fetch ] [ text "Fetch" ]
+        , div [] []
+        , viewPeople model.people
         ]
 
+
+viewPeople : WebData People -> Html Msg
+viewPeople model =
+    case model of
+        Loading ->
+            text "加载中"
+
+        Failure _ ->
+            text "加载失败"
+
+        Success people ->
+            people.content
+                |> List.map (viewPerson )
+                |> ul [styles commitList]
+
+        _ ->
+            text ""
+
+
+viewPerson : Person -> Html Msg
+viewPerson  person =
+   li [ styles card ]
+    [ h4 [] [ text person.name ]
+    ]
 
 decode : Decode.Decoder String
 decode =
     -- Decode.at [ "name" ] Decode.string
     Decode.string
 
+
 url : String
 url =
     "https://www.sina.com.cn"
 
 
-fetchTask : Task Http.Error String
+fetchTask : Task Http.Error People
 fetchTask =
-    Http.get url decode |> Http.toTask
+    Http.get url decodePeople |> Http.toTask
 
-fetch1 : Task Http.Error String
+
+fetch1 : Task Http.Error People
 fetch1 =
-    Http.get "http://168.35.6.12:8099/aic/api/people?page=1" decode
+    Http.get "http://168.35.6.12:8099/aic/api/people?page=1" decodePeople
         |> Http.toTask
 
-fetch0 : Task Http.Error String
+
+fetch0 : Task Http.Error People
 fetch0 =
-    Http.get "http://168.35.6.12:8099/aic/api/people?page=0" decode
+    Http.get "http://168.35.6.12:8099/aic/api/people?page=0" decodePeople
         |> Http.toTask
+
 
 {-| Task.perform Never make a error of Result,opposite to it,
      attempt will handle a result of (Result error a) which may fail in
-     elm runtime-}
+     elm runtime
+-}
 fetchCmd : Cmd Msg
 fetchCmd =
     -- Task.perform FetchSuccess fetchTask
-    Task.attempt FetchMsg (fetch0
-                          |> Task.andThen (\n -> fetch1))
-
+    Task.attempt FetchMsg
+        (fetch0
+            |> Task.andThen (\n -> fetch1)
+        )
 
 
 
@@ -89,19 +131,22 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Fetch ->
-            ( model, fetchCmd )
+            ( { model | people = Loading }, fetchCmd )
 
-        FetchMsg (Ok name) ->
-            ( name, Cmd.none)
+        FetchMsg (Ok res) ->
+            ( { model | people = Success res}, Cmd.none )
 
         FetchMsg (Err error) ->
-            ( toString error, Cmd.none )
-        -- FetchSuccess name ->
-            -- ( name, Cmd.none )
+            ( { model | title = toString error }
+            , Cmd.none
+            )
 
-        -- FetchError error ->
-            -- ( toString error, Cmd.none )
 
+
+-- FetchSuccess name ->
+-- ( name, Cmd.none )
+-- FetchError error ->
+-- ( toString error, Cmd.none )
 -- MAIN
 
 
