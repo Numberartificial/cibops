41dfadd9254daa636462b32adabe63a0a3f1e99d
边城道馆 <248866053@qq.com>
2017-04-01 10:32:10 +0800

    it's very easy to modify, dropdown done.

--- src/Pages/CibFetch.elm
+++ src/Pages/CibFetch.elm
@@ -1,14 +1,15 @@
-module Pages.CibFetch exposing(..)
+module Pages.CibFetch exposing (..)
 
 import Http
 import Html exposing (..)
-import Html.Attributes exposing (href)
+import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Task exposing (Task)
 
 import RemoteData.Http exposing (..)
 import RemoteData exposing (RemoteData(..), WebData)
 import Bootstrap.Button as Button exposing (..)
+import Bootstrap.Dropdown as Dropdown exposing (..)
 
 import Views.WebListView as ListView
 import Effects.Web as Resource exposing (..)
@@ -16,91 +17,156 @@ import Effects.Web as Resource exposing (..)
 
 -- MODEL
 
-type alias Model=
-    { people : ListView.Model Person
+
+type alias Model =
+    { drop : Dropdown.State
+    , people : ListView.Model Person
     }
+
 
 
 -- INIT
 
 
-init : (Model , Cmd Msg)
+init : ( Model, Cmd Msg )
 init =
     let
         ( peopleModel, peopleCmd ) =
             ListView.init
     in
-        ( { people = peopleModel }
+        ( { drop = Dropdown.initialState
+          , people = peopleModel
+          }
         , Cmd.batch
-           [ Cmd.map PeopleMsg peopleCmd ]
+            [ Cmd.map PeopleMsg peopleCmd ]
         )
+
+
 
 -- UPDATE
 
-type Msg =
-    FetchPeople
+
+type Msg
+    = DropMsg Dropdown.State
+    | FetchPeople
     | PeopleMsg (ListView.Msg Person)
 
-mapWebData : ( a -> b ) -> WebData a -> WebData b
+
+mapWebData : (a -> b) -> WebData a -> WebData b
 mapWebData f webData =
     case webData of
-        Success a -> Success (f a)
-        Failure error -> Failure error
-        NotAsked -> NotAsked
-        Loading -> Loading
+        Success a ->
+            Success (f a)
 
+        Failure error ->
+            Failure error
+
+        NotAsked ->
+            NotAsked
+
+        Loading ->
+            Loading
 
 
 fetch =
-    getWithConfig defaultConfig "http://168.35.6.12:8099/aic/api/people"
-        (
-        (\wedata ->
-           mapWebData
-             (\people -> people.content) wedata
-            |> ListView.FetchMsg)
+    getWithConfig defaultConfig
+        "http://168.35.6.12:8099/aic/api/people"
+        ((\wedata ->
+            mapWebData
+                (\people -> people.content)
+                wedata
+                |> ListView.FetchMsg
+         )
         )
         decodePeople
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        DropMsg state ->
+            Tuple.mapFirst (\model -> { model | drop = state }) <|
+                updatePeople model ListView.Fetch
+
         FetchPeople ->
             updatePeople model ListView.Fetch
+
         PeopleMsg peopleMsg ->
             updatePeople model peopleMsg
 
-updatePeople : Model -> ListView.Msg Person -> ( Model, Cmd Msg)
+
+updatePeople : Model -> ListView.Msg Person -> ( Model, Cmd Msg )
 updatePeople model msg =
     let
         ( nextModel, nextCmd ) =
             ListView.update fetch msg model.people
     in
-        ( { model | people = nextModel
-                }
-          , Cmd.map PeopleMsg nextCmd )
+        ( { model
+            | people = nextModel
+          }
+        , Cmd.map PeopleMsg nextCmd
+        )
+
 
 
 -- VIEW
 
 
-view : Model-> Html Msg
+view : Model -> Html Msg
 view model =
     div []
-        [ Button.button
-              [Button.primary,
-              Button.attrs
-                [ onClick (FetchPeople)]]
-              [ text "fetch people"]
-        ,
-          ListView.custom viewPerson
-              model.people
+        [ viewDrop model
+        , Button.button
+            [ Button.primary
+            , Button.attrs
+                [ onClick (FetchPeople) ]
+            ]
+            [ text "fetch people" ]
+        , ListView.custom viewPerson
+            model.people
+        ]
+
+
+viewDrop : Model -> Html Msg
+viewDrop model =
+    div []
+        [ Dropdown.dropdown
+            model.drop
+            { options = [ Dropdown.alignMenuRight ]
+            , toggleMsg = DropMsg
+            , toggleButton =
+                Dropdown.toggle [ Button.roleLink ] [ text "datasource" ]
+            , items =
+                [ Dropdown.buttonItem [ onClick FetchPeople ] [ text "fetch people" ]
+                , Dropdown.buttonItem [ onClick FetchPeople ] [ text "Item 2" ]
+                , Dropdown.divider
+                , Dropdown.header [ text "Silly items" ]
+                , Dropdown.buttonItem [ class "disabled" ] [ text "DoNothing1" ]
+                , Dropdown.buttonItem [] [ text "DoNothing2" ]
+                ]
+            }
+          -- etc
         ]
 
 
 viewPerson : Person -> Html Msg
 viewPerson person =
     div []
-        [ text (toString person.name)]
+        [ text (toString person.name) ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+sub : Model -> Sub Msg
+sub model =
+    Sub.batch
+        [ Dropdown.subscriptions
+            model.drop
+            DropMsg
+        ]
+
 
 
 -- MAIN
@@ -112,5 +178,5 @@ main =
         { init = init
         , view = view
         , update = update
-        , subscriptions = (always Sub.none)
+        , subscriptions = sub
         }
